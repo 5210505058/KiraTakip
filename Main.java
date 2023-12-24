18b3b92c434cb78034f23d23f99f3dc60168e1b6
5210505058 <127990796+5210505058@users.noreply.github.com>
2023-12-24 21:46:59 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bina binam = new Bina("Lorem Ipsum", 31);
+
+
+        EvSahibi sahip1 = new EvSahibi("Name1", "Surname1");
+        EvSahibi sahip2 = new EvSahibi("Name2", "Surname2");
+
+        Daire daire1 = new Daire("Ut enim ad minim veniam, quis nostrud exercitation ullamco ", 1);
+        Daire daire2 = new Daire("Lorem ipsum dolor sit amet, consectetur adipiscing elit", 41);
+
+        sahip1.daireEkle(daire1);
+        sahip2.daireEkle(daire2);
+
+        binam.Ekle(daire1);
+        binam.Ekle(daire2);
+
+        sahip1.Goster();
+        sahip2.Goster();
+        System.out.println("\n");
+
+        Kiraci kiraci1 = new Kiraci("Name1", "Surname1", sahip1);
+        Kiraci kiraci2 = new Kiraci("Name2", "Surname2", sahip2);
+
+        kiraci1.Goster();
+        kiraci2.Goster();
+    }
+}
