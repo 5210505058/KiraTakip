5f9ef202dba65ffd9c1fd543718d37b5451847eb
5210505058 <127990796+5210505058@users.noreply.github.com>
2023-12-24 22:16:22 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -5,15 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         // Bir bina örneği oluşturuluyor
-        Bina binam = new Bina("Lorem Ipsum", 17);
+        Bina binam = new Bina("Örnek Konutlari", 17);
 
         // İki ev sahibi örneği oluşturuluyor
         EvSahibi sahip1 = new EvSahibi("Ad1", "Soyad1");
         EvSahibi sahip2 = new EvSahibi("Ad2", "Soyad2");
 
         // İki daire örneği oluşturuluyor
-        Daire daire1 = new Daire("Ut enim ad minim veniam, quis nostrud exercitation ullamco", 5);
-        Daire daire2 = new Daire("Lorem ipsum dolor sit amet, consectetur adipiscing elit", 21);
+        Daire daire1 = new Daire("Yesil Konutları daire ", 5);
+        Daire daire2 = new Daire("Örnek Konutlari kapi girisi", 21);
 
         // Ev sahiplerine daireler ekleniyor
         sahip1.daireEkle(daire1);
