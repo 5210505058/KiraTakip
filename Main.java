956ad5bd329e0b3e39259649298a54317bfb412d
5210505058 <127990796+5210505058@users.noreply.github.com>
2023-12-24 21:57:33 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Bina binam = new Bina("Lorem Ipsum", 31);
+        Bina binam = new Bina("Lorem Ipsum", 17);
 
 
         EvSahibi sahip1 = new EvSahibi("Name1", "Surname1");
         EvSahibi sahip2 = new EvSahibi("Name2", "Surname2");
 
-        Daire daire1 = new Daire("Ut enim ad minim veniam, quis nostrud exercitation ullamco ", 1);
-        Daire daire2 = new Daire("Lorem ipsum dolor sit amet, consectetur adipiscing elit", 41);
+        Daire daire1 = new Daire("Ut enim ad minim veniam, quis nostrud exercitation ullamco ", 5);
+        Daire daire2 = new Daire("Lorem ipsum dolor sit amet, consectetur adipiscing elit", 21);
 
         sahip1.daireEkle(daire1);
         sahip2.daireEkle(daire2);
